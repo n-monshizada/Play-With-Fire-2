ece0081cbf627a95a00f5f501d82eba3e6719d13
Naweed Monshi-Zada <109760044+n-monshizada@users.noreply.github.com>
2022-07-22 07:29:31 -0400

    These are all files for the final project of C++.

---
+++ settings.h
@@ -0,0 +1,19 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+namespace sizes {
+    enum gameStats {
+        Rows = 15,
+        Columns = 15,
+        FieldSize = 50,
+        Players = 2
+    };
+    enum playerStats {
+        Bombs = 3,
+        ExplosionRange = 2,
+        Health = 3
+    };
+}
+
+
+#endif // SETTINGS_H
